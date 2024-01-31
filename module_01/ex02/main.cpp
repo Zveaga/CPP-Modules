0611e2ab7d7bf3ee4d5e6a76b57a5fd94e3b0b88
Rares <rares.rares.cox@gmail.com>
2024-01-31 15:10:39 +0100

    01_02 complete

---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: coxer <coxer@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/31 14:49:33 by coxer         #+#    #+#                 */
+/*   Updated: 2024/01/31 15:10:00 by coxer         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << &string << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << string << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+}
