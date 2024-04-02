fd8a80d589254d0717dbbb1b268e44e999ce9098
Rares <rares.rares.cox@gmail.com>
2024-04-02 16:15:21 +0200

    05-00 done

--- module_05/ex00/main.cpp
+++ module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: coxer <coxer@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/19 16:35:43 by coxer         #+#    #+#                 */
-/*   Updated: 2024/03/11 15:06:41 by coxer         ########   odam.nl         */
+/*   Updated: 2024/04/02 16:14:45 by coxer         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,37 @@ int main(void)
 {
 	std::cout << "\n============CONSTRUCTION=============\n";
 	
-	std::cout << "\n------------VALID GRADE------------\n\n";
+	std::cout << "\n------------VALID GRADE----------\n\n";
 	
 	Bureaucrat bureaucrat("CLERK", 10);
 	
-	std::cout << "\n------------GRADE TOO LOW--------\n\n";
+	std::cout << "\n-----------GRADE TOO LOW---------\n\n";
 	
 	try
 	{
-		Bureaucrat manager("MANAGER", 1000);
+		Bureaucrat manager("MANAGER", 151);
 	}
 	catch (const std::exception &e)
 	{
 		std::cerr << "Error constructing the bureaucrat: " << e.what() << std::endl;
 	}
 	
-	std::cout << "\n----------GRADE TOO HIGH----------\n\n";
+	std::cout << "\n-----------GRADE TOO HIGH----------\n\n";
 	
 	try
 	{
-		Bureaucrat director("DIRECTOR", -5);
+		Bureaucrat director("DIRECTOR", 0);
 	}
 	catch (const std::exception &e)
 	{
-		std::cerr << "Error constructing the bureaucrat " << e.what() << std::endl;	
+		std::cerr << "Error constructing the bureaucrat: " << e.what() << std::endl;	
 	} 
 
-	std::cout << "\n============INCREMENT=============\n";
+	std::cout << "\n============INCREMENT=============\n\n";
 	
 	bureaucrat.setGrade(2);
+	std::cout << bureaucrat;
+	
 	try
 	{
 		bureaucrat.incrementGrade();
@@ -53,12 +55,13 @@ int main(void)
 	catch (const std::exception &e)
 	{
 		std::cerr << "Error while incrementing : " << e.what() << std::endl;	
-		std::cout << bureaucrat;
 	}
-	
-	std::cout << "\n============DECREMENT=============\n";
+	std::cout << bureaucrat;
+	std::cout << "\n============DECREMENT=============\n\n";
 	
 	bureaucrat.setGrade(148);
+	std::cout << bureaucrat;
+	
 	try
 	{
 		bureaucrat.decrementGrade();
@@ -71,9 +74,9 @@ int main(void)
 	catch (const std::exception &e)
 	{
 		std::cerr << "Error while decrementing : " << e.what() << std::endl;	
-		std::cout << bureaucrat;
 	}
 	
+	std::cout << bureaucrat;
 	
 	std::cout << "\n=======OPERATOR << OVERLOAD=======\n";
 	
@@ -81,6 +84,5 @@ int main(void)
 	std::cout << bureaucrat;
 	
 	std::cout << "\n======================END======================\n";
-	//std::cout << "\n--------------------------\n";
 	return (0);
 }
