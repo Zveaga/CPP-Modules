f02bb0b542dfcb5181e4c64cb4ac047b5849f285
Zveaga <98222905+Zveaga@users.noreply.github.com>
2024-05-03 20:59:43 +0200

    still working on 06-00

--- module_06/ex00/ScalarConverter.cpp
+++ module_06/ex00/ScalarConverter.cpp
@@ -31,143 +31,383 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &object)
 
 // --Non-Members-- //
 
-enum class LiteralType
-{
-	CHAR,
-	INT,
-	FLOAT,
-	DOUBLE,
-	IMPOSSIBLE,
-	NON_DISPLAYABLE,
-};
-
 bool isChar(const std::string &value)
 {
-	if (value.length() == 1 && !std::isdigit(value[0]))
-	{
-		return (true);
-	}
-	return (false);
-}
+	if (value.empty() || value.length() != 1 || std::isdigit(value[0]))
+		return (false);
 
-bool isDisplayable(char ch)
-{
-	return (ch >= 32 && ch <= 126);
+	int i_value = static_cast<int>(value[0]);
+	if (i_value < 0 || i_value > 127)
+		return (false);
+	return (true);
 }
 
 bool isInt(const std::string &value)
 {
-	size_t pos = 0;
-	try
-	{
-		std::stoi(value, &pos);
-		if (pos == value.length())
-			return (true);
-	}
-	catch (const std::invalid_argument &e)
-	{
-		std::cout << "Invalid argument: " << e.what() << "\n";
-	}
-	catch (const std::out_of_range &e)
-	{
-		std::cout << "Out of range: " << e.what() << "\n";
-	}
-	return (false);
+	
 }
 
 bool isFloat(const std::string &value)
 {
-	size_t pos = 0;
-	try
-	{
-		std::stof(value, &pos);
-		if (pos == value.length())
-			return (true);
-	}
-	catch (const std::invalid_argument &e)
-	{
-		std::cout << "Invalid argument: " << e.what() << "\n";
-	}
-	catch (const std::out_of_range &e)
-	{
-		std::cout << "Out of range: " << e.what() << "\n";
-	}
-	return (false);
+	
 }
 
 bool isDouble(const std::string &value)
 {
-	size_t pos = 0;
-	try
-	{
-		std::stod(value, &pos);
-		if (pos == value.length())
-			return (true);
-	}
-	catch (const std::invalid_argument &e)
-	{
-		std::cout << "Invalid argument: " << e.what() << "\n";
-	}
-	catch (const std::out_of_range &e)
-	{
-		std::cout << "Out of range: " << e.what() << "\n";
-	}
-	return (false);
+	
 }
 
-LiteralType detectType(const std::string &value)
+
+void detectType(const std::string &value)
 {
-	if (isChar(value))
-	{
-		if (!isDisplayable(value[0]))
-			return (LiteralType::NON_DISPLAYABLE);
-		return (LiteralType::CHAR);
-	}
-	if (isInt(value))
-		return (LiteralType::INT);
-	if (isFloat(value))
-		return (LiteralType::FLOAT);
-	if (isDouble(value))
-		return (LiteralType::DOUBLE);
-	return (LiteralType::IMPOSSIBLE);
+	// try
+	// {
+		if (isChar(value))
+		{
+			std::cout << "CHAR\n";
+			return ;
+			// return (LiteralType::CHAR);
+		}
+		else if (isInt(value))
+		{
+			std::cout <<  "INT\n";
+			return ;
+			// return (LiteralType::INT);
+		}
+		else if (isFloat(value))
+		{
+			std::cout <<  "FLOAT\n";
+			return ;
+			// return (LiteralType::FLOAT);
+		}
+		else if (isDouble(value))
+		{
+			std::cout <<  "DOUBLE\n";
+			return ;
+
+			// return (LiteralType::DOUBLE);
+		}
+	// }
+	// catch (const std::invalid_argument &e)
+	// {
+	// 	std::cout << "Invalid argument(DOUBLE): " << e.what() << "\n";
+	// }
+	// catch (const std::out_of_range &e)
+	// {
+	// 	std::cout << "Out of range(DOUBLE): " << e.what() << "\n";
+	// }
+	// return (LiteralType::IMPOSSIBLE);
 }
+
 
 // --Member Functions-- //
 
 void ScalarConverter::convert(const std::string value)
 {
+	detectType(value);
 
-	LiteralType type = detectType(value);
-	switch (type)
-	{
-		case LiteralType::CHAR:
-			std::cout << "Type: CHAR\n";
-			break ;
-		case LiteralType::INT:
-			std::cout << "Type: INT\n";
-			break ;
-		case LiteralType::FLOAT:
-			std::cout << "Type: FLOAT\n";
-			break ;
-		case LiteralType::DOUBLE:
-			std::cout << "Type: DOUBLE\n";
-			break ;
-		case LiteralType::IMPOSSIBLE:
-			std::cout << "Type: IMPOSSIBLE\n";
-			break ;
-		case LiteralType::NON_DISPLAYABLE:
-			std::cout << "Type: CHAR NON-DISPLAYABLE\n";
-			break ;
-	}
-	// std::cout << "Original type: " << typeid(value).name() << "\n";
 	// char	asChar;
 	// int		asInt;
 	// float	asFloat;
 	// double	asDouble;
 
+	// LiteralType type = detectType(value);
+	// switch (type)
+	// {
+	// 	case LiteralType::CHAR:
+	// 		std::cout << "Type: CHAR\n";
+	// 		break ;
+	// 	case LiteralType::INT:
+	// 		std::cout << "Type: INT\n";
+	// 		break ;
+	// 	case LiteralType::FLOAT:
+	// 		std::cout << "Type: FLOAT\n";
+	// 		break ;
+	// 	case LiteralType::DOUBLE:
+	// 		std::cout << "Type: DOUBLE\n";
+	// 		break ;
+	// 	case LiteralType::IMPOSSIBLE:
+	// 		std::cout << "Type: IMPOSSIBLE\n";
+	// 		break ;
+	// 	case LiteralType::NON_DISPLAYABLE:
+	// 		std::cout << "Type: CHAR NON-DISPLAYABLE\n";
+	// 		break ;
+	// }
 	// std::cout
 	// 		<< "char: " << asChar << "\n"
 	// 		<< "int: " << asInt << "\n"
 	// 		<< "float : " << asFloat << "\n"
 	// 		<< "double: " << asDouble << "\n";
 }
+
+// LiteralType detectType(const std::string &value)
+// {
+// 	char	asChar = '\0';
+// 	int		asInt = 0;
+// 	float	asFloat = 0;
+// 	double	asDouble = 0;
+// 	try
+// 	{
+// 		if (convertToChar(value))
+// 		{
+// 			if (!isDisplayable(value[0]))
+// 				return (LiteralType::NON_DISPLAYABLE);
+// 			return (LiteralType::CHAR);
+// 		}
+// 		if (convertToInt(value))
+// 			return (LiteralType::INT);
+// 		if (convertToFloat(value))
+// 			return (LiteralType::FLOAT);
+// 		if (convertToDouble(value))
+// 			return (LiteralType::DOUBLE);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(DOUBLE): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(DOUBLE): " << e.what() << "\n";
+// 	}
+// 	return (LiteralType::IMPOSSIBLE);
+// }
+
+
+
+
+// bool convertToChar(const std::string &value)
+// {
+// 	if (value.length() == 1 && !std::isdigit(value[0]))
+// 	{
+// 		if (!isDisplayable(value[0]))
+// 		{
+// 			std::cout <<  "char: Non displayable\n";
+// 			return (false);
+// 		}
+// 		return (true);
+// 	}
+// 	return (false);
+// }
+
+
+// bool convertToInt(const std::string &value, t_literals &literals)
+// {
+// 	size_t pos = 0;
+// 	try
+// 	{
+// 		literals.asInt = std::stoi(value, &pos);
+// 		if (pos == value.length())
+// 			return (true);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(INT): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(INT): " << e.what() << "\n";
+// 	}
+	
+// 	// catch (const std::invalid_argument &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+// 	// catch (const std::out_of_range &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+	
+// 	return (false);
+// }
+
+// bool convertToFloat(std::string value, t_literals &literals)
+// {
+// 	if (value.back() == 'f' || value.back() == 'F')
+// 		value.pop_back();
+// 	else if (value.back() != 'f' || value.back() != 'F')
+// 		return (false);
+// 	size_t pos = 0;
+// 	try
+// 	{
+// 		literals.asFloat = std::stof(value, &pos);
+// 		if (pos == value.length())
+// 			return (true);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(FLOAT): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(FLOAT): " << e.what() << "\n";
+// 	}
+	
+// 	// catch (const std::invalid_argument &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+// 	// 	catch (const std::out_of_range &)
+// 	// 	{
+// 	// 		throw ;
+// 	// 	}
+
+// 	return (false);
+// }
+
+// bool convertToDouble(const std::string &value, t_literals &literals)
+// {
+// 	if (value.back() == 'f' || value.back() == 'F')
+// 		return (false);
+// 	size_t pos = 0;
+// 	try
+// 	{
+// 		literals.asDouble = std::stod(value, &pos);
+// 		if (pos == value.length())
+// 			return (true);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(DOUBLE): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(DOUBLE): " << e.what() << "\n";
+// 	}
+
+
+
+// std::string values[4] = {};
+
+// enum class LiteralType
+// {
+// 	CHAR,
+// 	INT,
+// 	FLOAT,
+// 	DOUBLE,
+// 	IMPOSSIBLE,
+// 	NON_DISPLAYABLE,
+// };
+
+// typedef struct s_literals
+// {
+// 	char	asChar = '\0';
+// 	int		asInt = 0;
+// 	float	asFloat = 0.0f;
+// 	double	asDouble = 0.0;
+// }			t_literals;
+
+// bool isDisplayable(char ch)
+// {
+// 	return (ch >= 32 && ch <= 126);
+// }
+
+// bool convertToChar(const std::string &value)
+// {
+// 	if (value.length() == 1 && !std::isdigit(value[0]))
+// 	{
+// 		if (!isDisplayable(value[0]))
+// 		{
+// 			std::cout <<  "char: Non displayable\n";
+// 			return (false);
+// 		}
+// 		return (true);
+// 	}
+// 	return (false);
+// }
+
+
+// bool convertToInt(const std::string &value)
+// {
+// 	size_t pos = 0;
+// 	try
+// 	{
+// 		std::stoi(value, &pos);
+// 		if (pos == value.length())
+// 			return (true);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(INT): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(INT): " << e.what() << "\n";
+// 	}
+	
+// 	// catch (const std::invalid_argument &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+// 	// catch (const std::out_of_range &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+	
+// 	return (false);
+// }
+
+// bool convertToFloat(std::string value)
+// {
+// 	if (value.back() == 'f' || value.back() == 'F')
+// 		value.pop_back();
+// 	else if (value.back() != 'f' || value.back() != 'F')
+// 		return (false);
+// 	size_t pos = 0;
+// 	try
+// 	{
+// 		std::stof(value, &pos);
+// 		if (pos == value.length())
+// 			return (true);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(FLOAT): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(FLOAT): " << e.what() << "\n";
+// 	}
+	
+// 	// catch (const std::invalid_argument &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+// 	// 	catch (const std::out_of_range &)
+// 	// 	{
+// 	// 		throw ;
+// 	// 	}
+
+// 	return (false);
+// }
+
+// bool convertToDouble(const std::string &value)
+// {
+// 	if (value.length() != 1 && (value.back() == 'f' || value.back() == 'F'))
+// 		return (false);
+// 	size_t pos = 0;
+// 	try
+// 	{
+// 		std::stod(value, &pos);
+// 		if (pos == value.length())
+// 			return (true);
+// 	}
+// 	catch (const std::invalid_argument &e)
+// 	{
+// 		std::cout << "Invalid argument(DOUBLE): " << e.what() << "\n";
+// 	}
+// 	catch (const std::out_of_range &e)
+// 	{
+// 		std::cout << "Out of range(DOUBLE): " << e.what() << "\n";
+// 	}
+	
+// 	// catch (const std::invalid_argument &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+// 	// catch (const std::out_of_range &)
+// 	// {
+// 	// 	throw ;
+// 	// }
+
+// 	return (false);
+// }
--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rares <rares@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/29 19:27:02 by rares         #+#    #+#                 */
-/*   Updated: 2024/05/02 15:02:49 by coxer         ########   odam.nl         */
+/*   Updated: 2024/05/03 20:35:49 by rares         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@ int main(int argc, char **argv)
 		std::cerr << "Error! Program accepts 1 parameter only!\n";
 		std::exit(EXIT_FAILURE);
 	}
-	// int a = 1;
-	// float b = 1.0;
-	// double c = 2.0;
 	
 	ScalarConverter::convert(argv[1]);
 	return (0);
